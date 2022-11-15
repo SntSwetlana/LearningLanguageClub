423d128da25bbe5988786e797f84984089dd367e
SntSwetlana <101958275+SntSwetlana@users.noreply.github.com>
2022-11-15 16:09:26 +0300

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
         'airbnb',
+        'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -14,6 +16,8 @@ module.exports = {
     },
     plugins: [
         'react',
+        '@typescript-eslint',
+        'i18next',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -40,6 +44,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
         'max-len': ['error', {
             ignoreComments: true,
             code: 100,
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,11 +4,10 @@ export function classNames(cls:string, mods: Mods = {}, additional: string[] = [
     return [
         cls,
         ...additional.filter(Boolean),
-        Object.entries(mods)
-            .filter(([className, value]) => Boolean(value))
+        ...Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
             .map(([className]) => className),
-    ]
-        .join(' ');
+    ].join(' ');
 }
 
 classNames('remove-btn', { hovered: true, selectable: true, red: false }, ['pdg']);
